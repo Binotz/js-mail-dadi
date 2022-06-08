f593a2f4af928691bec5948fde3e0dcca1936374
Binotz <binotto.riccardo@gmail.com>
2022-06-08 15:48:30 +0200

    Dadi done

---
+++ dadi/js/script.js
@@ -0,0 +1,33 @@
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+//definizione variabili e elementi DOM
+let playerNumber = 0;
+let PCNumber = 0;
+let msg = '';
+const playerNumberText = document.getElementById('player-number');
+const pcNumberText = document.getElementById('pc-number');
+const playBtn = document.getElementById('play-btn');
+const result = document.getElementById('result');
+
+playBtn.addEventListener('click',
+function(){
+        //al click, genero 2 numeri a caso, per il player e per il pc
+        playerNumber = Math.floor(Math.random() * 6) + 1;
+        PCNumber = Math.floor(Math.random() * 6) + 1;
+        console.log(playerNumber, PCNumber);
+        //controllo il risultato
+        if (playerNumber > PCNumber){
+            msg = 'Hai vinto, il PC ha perso!';
+        } else if(PCNumber > playerNumber){
+            msg = 'Il PC ha vinto, hai perso!';
+        } else {
+            msg = 'Oh oh! Parità!'
+        }
+        //Mostro il numero del giocatore, del pc e il risultato
+        playerNumberText.innerHTML = playerNumber;
+        pcNumberText.innerHTML = PCNumber;
+        result.innerHTML = msg;
+    }
+);
